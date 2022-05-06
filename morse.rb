b8ae9250114ac2bb64197b374b976cbefc1842a9
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-05-06 09:05:17 -0600

    Improve code readability and fix linters

--- morse.rb
+++ morse.rb
@@ -1,18 +1,21 @@
+# morse decoder class
 class MorseDecoder
-  @@letters_hash = {
-    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
-    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
-    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
-    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
-    '--..' => 'Z'
-  }
-
-  def decode_char(str = '')
-    @@letters_hash[str] ||= ''
+  def initialize
+    @letters_hash = {
+      '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+      '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+      '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+      '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+      '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+      '--..' => 'Z'
+    }
   end
 
-# Method that decode a word by calling decode_char method
+  def decode_char(str = '')
+    @letters_hash[str] ||= ''
+  end
+
+  # Method that decode a word by calling decode_char method
   def decode_word(param1)
     param = param1
     text = ''
@@ -20,23 +23,18 @@ class MorseDecoder
     text
   end
 
-# Method that decode a sentence by calling decode_word method
+  # Method that decode a sentence by calling decode_word method
   def decode(param2)
     param1 = param2
     text = ''
     param1.split('   ').each { |word| text << "#{decode_word(word)} " }
     text
   end
-
 end
 
 # Test
 
 morse_decoder = MorseDecoder.new
-puts morse_decoder.decode_char('...-')
-puts morse_decoder.decode_char('x')
-# => "V"
-# => ""
 
 puts morse_decoder.decode('-- -.--   -. .- -- .')
 
