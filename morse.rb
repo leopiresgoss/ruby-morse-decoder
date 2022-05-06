2c382bff9816371f520a6165bdbf792b7a929c19
ForHemer <foreste.hemerson@gmail.com>
2022-05-06 09:41:53 -0400

    Add decode_word and decode methods

--- morse.rb
+++ morse.rb
@@ -12,10 +12,32 @@ class MorseDecoder
     @@letters_hash[str] ||= ''
   end
 
+# Method that decode a word by calling decode_char method
+  def decode_word(param1)
+    param = param1
+    text = ''
+    param.split.each { |letter| text << decode_char(letter) }
+    text
+  end
+
+# Method that decode a sentence by calling decode_word method
+  def decode(param2)
+    param1 = param2
+    text = ''
+    param1.split('   ').each { |word| text << "#{decode_word(word)} " }
+    text
+  end
+
 end
 
+# Test
+
 morse_decoder = MorseDecoder.new
-morse_decoder.decode_char('...-')
-morse_decoder.decode_char('x')
+puts morse_decoder.decode_char('...-')
+puts morse_decoder.decode_char('x')
 # => "V"
 # => ""
+
+puts morse_decoder.decode('-- -.--   -. .- -- .')
+
+puts morse_decoder.decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
